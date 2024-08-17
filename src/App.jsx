26a8551146f6bbb0018bfbed0cd1d6edd992236a
arpitbhau <arpitnikhade20@gmail.com>
2024-08-17 21:40:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// JAI SHREE RAM
+
+import React from 'react'
+import Loader from './components/Loader'
+
+function App() {
+  return (
+    <>
+  
+    <Loader></Loader>
+    
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Loader.jsx
@@ -0,0 +1,16 @@
+// JAI SHREE RAM
+
+import React from 'react'
+import "../../public/css/Loader.css"
+
+function Loader() {
+    return (
+        <div className="loader w-full h-screen overflow-hidden relative font-['jollyLodger']">
+            <div className="wrapper">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Loader
