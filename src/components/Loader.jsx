3f65c39d729aa4e78bfa14c4dfefc92fb09df9e0
arpitbhau <arpitnikhade20@gmail.com>
2024-08-21 19:10:44 +0530

    21-08-2024--first

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,10 +5,9 @@ import "../../public/css/Loader.css"
 
 function Loader() {
     return (
-        <div className="loader w-full h-screen overflow-hidden relative font-['jollyLodger']">
-            <div className="wrapper">
-                
-            </div>
+        <div className="loader w-full h-screen overflow-hidden flex flex-col items-center justify-center relative font-['jollyLodger']">
+            <div className="wrapper w-[99.2%] h-[98.5%] bg-[#111] rounded-xl absolute"></div>
+            <div className="lMain bg-red-500 w-1/2 h-1/2 relative z-1000000 rounded-full"></div>
         </div>
     )
 }
